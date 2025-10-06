84361907df571dd568e816c559140492624c210f
Shubham Vishwakarma <sv8112004@gmail.com>
2025-10-06 22:14:48 +0530

    Project Done

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema, reviewSchema } = require("./schema.js");
 const Review = require("./models/review.js");
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
@@ -26,8 +27,9 @@ const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 const profileRoutes = require('./routes/profile');
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
+const dbURL = process.env.ATLASDB_URL;
 main()
     .then(() => {
         console.log("Connected to MongoDB");
@@ -36,7 +38,7 @@ main()
         console.log("Error connecting to MongoDB", err);
     });
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbURL);
 }
 
 app.set("view engine", "ejs");
@@ -46,8 +48,19 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+const store = MongoStore.create({
+    mongoUrl:dbURL,
+    crypto:{
+        secret:process.env.SECRET
+    },
+    touchAfter : 24*3600,
+});
+store.on("error",()=>{
+    console.log("ERROR in MONGO SESSION STORE",err);
+});
 const sessionOptions = {
-    secret: "mysupersecretcode",
+    store,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
